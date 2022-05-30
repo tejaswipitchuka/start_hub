81a09e2d167119f99f60325d46802b7f299dd846
bahulapatharlapalli <92917420+bahulapatharlapalli@users.noreply.github.com>
2022-05-30 15:14:51 +0530

    commit

--- start_hub_mysql_create.sql
+++ start_hub_mysql_create.sql
@@ -1,68 +1,68 @@
 CREATE TABLE `innovator` (
-	`first_name` varchar(50) NOT NULL,
-	`last_name` varchar(50) NOT NULL,
-	`email_id` varchar(50) NOT NULL,
-	`password` varchar(255) NOT NULL,
-	`mobile_no` varchar(15) NOT NULL,
-	`qualification` varchar(50) NOT NULL,
-	`linkedin` varchar(255) NOT NULL
+	`first_name` varchar(50) NULL,
+	`last_name` varchar(50) NULL,
+	`email_id` varchar(50) NULL,
+	`password` varchar(255) NULL,
+	`mobile_no` varchar(15) NULL,
+	`qualification` varchar(50) NULL,
+	`linkedin` varchar(255) NULL
 );
 
 CREATE TABLE `investor` (
-	`first_name` varchar(50) NOT NULL,
-	`last_name` varchar(50) NOT NULL,
-	`email_id` varchar(50) NOT NULL,
-	`password` varchar(255) NOT NULL,
-	`mobile_no` varchar(15) NOT NULL,
-	`company` varchar(50) NOT NULL,
-	`linkedin` varchar(255) NOT NULL,
-	`pancard` varchar(15) NOT NULL
+	`first_name` varchar(50) NULL,
+	`last_name` varchar(50) NULL,
+	`email_id` varchar(50) NULL,
+	`password` varchar(255) NULL,
+	`mobile_no` varchar(15) NULL,
+	`company` varchar(50) NULL,
+	`linkedin` varchar(255) NULL,
+	`pancard` varchar(15) NULL
 );
 
 CREATE TABLE `mentor` (
-	`first_name` varchar(50) NOT NULL,
-	`last_name` varchar(50) NOT NULL,
-	`email_id` varchar(50) NOT NULL,
-	`password` varchar(255) NOT NULL,
-	`mobile_no` varchar(15) NOT NULL,
-	`company` varchar(50) NOT NULL,
-	`linkedin` varchar(255) NOT NULL,
-	`qualification` varchar(50) NOT NULL,
-	`specialization` varchar(255) NOT NULL
+	`first_name` varchar(50) NULL,
+	`last_name` varchar(50) NULL,
+	`email_id` varchar(50) NULL,
+	`password` varchar(255) NULL,
+	`mobile_no` varchar(15) NULL,
+	`company` varchar(50) NULL,
+	`linkedin` varchar(255) NULL,
+	`qualification` varchar(50) NULL,
+	`specialization` varchar(255) NULL
 );
 
 CREATE TABLE `organization` (
-	`email_id` varchar(50) NOT NULL,
-	`password` varchar(255) NOT NULL,
-	`mobile_no` varchar(15) NOT NULL,
-	`company` varchar(50) NOT NULL,
-	`linkedin` varchar(255) NOT NULL,
-	`pancard` varchar(15) NOT NULL
+	`email_id` varchar(50) NULL,
+	`password` varchar(255) NULL,
+	`mobile_no` varchar(15) NULL,
+	`company` varchar(50) NULL,
+	`linkedin` varchar(255) NULL,
+	`pancard` varchar(15) NULL
 );
 
 CREATE TABLE `idea_post` (
-	`title` varchar(100) NOT NULL,
-	`author` varchar(100) NOT NULL,
-	`description` TEXT(600) NOT NULL,
-	`upvotes` INT(10) NOT NULL,
-	`comment` TEXT(100) NOT NULL,
-	`sub_title` TEXT(100) NOT NULL,
-	`date` DATE(3) NOT NULL
+	`title` varchar(100) NULL,
+	`author` varchar(100) NULL,
+	`description` TEXT(600) NULL,
+	`upvotes` INT(10) NULL,
+	`comment` TEXT(100) NULL,
+	`subtitle` TEXT(100) NULL,
+	`date` DATE NULL
 );
 
 CREATE TABLE `blogs` (
-	`title` varchar(100) NOT NULL,
-	`author` varchar(100) NOT NULL,
-	`description` TEXT(600) NOT NULL,
-	`likes` INT(10) NOT NULL,
-	`date` DATE(3) NOT NULL
+	`title` varchar(100) NULL,
+	`author` varchar(100) NULL,
+	`description` TEXT(600) NULL,
+	`likes` INT(10) NULL,
+	`date` DATE NULL
 );
 
 CREATE TABLE `chatroom` (
-	`sender` varchar(100) NOT NULL,
-	`receiver` varchar(100) NOT NULL,
-	`message` TEXT(600) NOT NULL,
-	`date` TIMESTAMP NOT NULL
+	`sender` varchar(100) NULL,
+	`receiver` varchar(100) NULL,
+	`message` TEXT(600) NULL,
+	`date` TIMESTAMP NULL
 );
 
 
